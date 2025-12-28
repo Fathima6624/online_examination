cc0761b6dfff9e6802d799bbda5f993c02fd3c1e
fathima Nasrin <fathimanasrink624@gmail.com>
2025-12-28 15:40:59 +0530

    updated

--- onlineexam/settings.py
+++ onlineexam/settings.py
@@ -83,9 +83,9 @@ import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=False,
     )
 }
 
