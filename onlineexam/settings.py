25af7ff5aa1e3a56fe96db9420fff61028e55c1f
fathima Nasrin <fathimanasrink624@gmail.com>
2025-12-28 20:37:02 +0530

    Switch to SQLite and remove PostgreSQL

--- onlineexam/settings.py
+++ onlineexam/settings.py
@@ -78,17 +78,16 @@ WSGI_APPLICATION = 'onlineexam.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-import dj_database_url
-import os
-
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+
 
 
 # Password validation
